a8f1bf5c6625da43a0d416c75971bc50badd2cc0
Polygonal Graphics <154336865+Roman-Stanuch@users.noreply.github.com>
2025-01-25 10:24:58 -0600

    Added a Basic Window Loop

--- Terraria-SDL3/src/main.cpp
+++ Terraria-SDL3/src/main.cpp
@@ -1,7 +1,96 @@
-﻿#include <iostream>
+﻿
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <string>
 
-int main()
+constexpr int kScreenWidth{ 640 };
+constexpr int kScreenHeight{ 480 };
+
+bool init();
+void close();
+
+SDL_Window* gWindow{ nullptr };
+SDL_Surface* gScreenSurface{ nullptr };
+SDL_Surface* gHelloWorld{ nullptr };
+
+bool init()
 {
-	std::cout << "Hello CMake." << std::endl;
-	return 0;
+    bool success{ true };
+
+    if (!SDL_Init(SDL_INIT_VIDEO))
+    {
+        SDL_Log("SDL could not initialize! SDL error: %s\n", SDL_GetError());
+        success = false;
+    }
+    else
+    {
+        gWindow = SDL_CreateWindow("SDL3 Tutorial: Hello SDL3", kScreenWidth, kScreenHeight, 0);
+        if (gWindow == nullptr)
+        {
+            SDL_Log("Window could not be created! SDL error: %s\n", SDL_GetError());
+            success = false;
+        }
+        else
+        {
+            gScreenSurface = SDL_GetWindowSurface(gWindow);
+        }
+    }
+
+    return success;
+}
+
+void close()
+{
+    SDL_DestroySurface(gHelloWorld);
+    gHelloWorld = nullptr;
+
+    SDL_DestroyWindow(gWindow);
+    gWindow = nullptr;
+    gScreenSurface = nullptr;
+
+    SDL_Quit();
+}
+
+int main(int argc, char* args[])
+{
+    int exitCode{ 0 };
+
+    if (!init())
+    {
+        SDL_Log("Unable to initialize program!\n");
+        exitCode = 1;
+    }
+    else
+    {
+        bool quit{ false };
+
+        SDL_Event e;
+        SDL_zero(e);
+
+        while (quit == false)
+        {
+            while (SDL_PollEvent(&e))
+            {
+                if (e.type == SDL_EVENT_QUIT)
+                {
+                    quit = true;
+                }
+                if (e.key.key == SDLK_E)
+                {
+                    quit = true;
+                }
+            }
+
+            //Fill the surface white
+            SDL_FillSurfaceRect(gScreenSurface, nullptr, SDL_MapSurfaceRGB(gScreenSurface, 0xFF, 0xFF, 0xFF));
+            //Render image on screen
+            SDL_BlitSurface(gHelloWorld, nullptr, gScreenSurface, nullptr);
+            //Update the surface
+            SDL_UpdateWindowSurface(gWindow);
+        }
+    }
+
+    close();
+
+    return exitCode;
 }
